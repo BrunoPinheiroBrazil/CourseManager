483f4efb1581451f9021ebbd1c2cc376f863e938
Bruno Brazil <Bruno_Brazil@Dell.com>
2021-10-17 00:51:39 -0300

    WIP - Started Integration Tests

---
+++ CourseManager.Integration.Tests/CourseManagerIntegrationTests.cs
@@ -0,0 +1,22 @@
+using Xunit;
+
+namespace CourseManager.Integration.Tests
+{
+  public class CourseManagerIntegrationTests
+  {
+
+    [Fact(DisplayName = "Insert Student [Success]")]
+    public void Insert_Student_Success()
+    {
+      //Arrange
+      
+
+      //Act
+
+
+      //Assert
+
+
+    }
+  }
+}
